996f14fd5a287d86c7b754f7fead75d603e561de
LinGena <intexgroop@gmail.com>
2025-04-15 17:00:50 +0300

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import requests
 from dotenv import load_dotenv
 from proxies.proxy_manager import get_proxies
@@ -5,105 +6,146 @@ import random
 from db.core import IsDbCreated
 from db.core import Db
 import json
-import time
-from utils.func import func_chunk_array
 from threading import Thread
-from utils.func import write_to_file_json
 
 
 load_dotenv(override=True)
 
-class GetTreadPagesContent():
+def func_chunk_array(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+class TreadGеtProducts():
     def run(self, threads_count: int = 10) -> None:
-        years = list(range(1980, 2026))
-        chunks = func_chunk_array(years, threads_count)
-        for i, chunk in enumerate(chunks):
-            print(f"Thread {i+1}: {chunk}")
-            thread = Thread(target=self.run_tread, args=(chunk,))
+        self.proxies = get_proxies()
+        model = Db()
+        sql = f'SELECT * FROM {model.table_tasks} WHERE status=1'
+        rows = model.select(sql)
+        countThread = round(len(rows) / int(threads_count)) + 1
+        threads = [Thread(target=self.run_tread, args=(chunk,)) for chunk in func_chunk_array(rows, countThread)]
+        for thread in threads:
             thread.start()
 
-    def run_tread(self, years: list) -> None:
-        GenerateTask().run(years)
+    def run_tread(self, rows: list) -> None:
+        GеtProducts(self.proxies).run(rows)
 
 
-
-class GenerateTask():
-    def __init__(self):
-        self.list_proxies = get_proxies()
+class GеtProducts():
+    def __init__(self, list_proxies:list):
+        self.list_proxies = list_proxies 
         self.model = Db()
 
-    def run(self, years: list):
-        for year in years:
-            try:
-                self.b2v4apikey = None
-                while not self.b2v4apikey:
-                    try:
-                        self.b2v4apikey = self.get_api_key()
-                    except Exception as ex:
-                        print(ex)
-                result = {}
-                result['year'] = year
-                result['status'] = 1
-                data = {
-                    "EquipmentClass":"Passenger Cars & Light Trucks",
-                    "EquipmentYear":str(year),
-                    "q":"",
-                    "CompressedSearch":False
-                    }
-                resp_makes = self.get_response(data)
-                makes = resp_makes.get('result',{}).get('columns',{}).get('EquipmentMake',{}).get('results')
-             
-                for make in makes:
-                    try:
-                        if "EquipmentModel" in data:
-                            del data["EquipmentModel"]
-                        data.update({"EquipmentMake":make['key']})
-                        result['make'] = make['text']
-                        resp_models = self.get_response(data)
-                        models = resp_models.get('result',{}).get('columns',{}).get('EquipmentModel',{}).get('results')
+    def run(self, resp_data_list):
+        for resp_data in resp_data_list:
+            id = resp_data[0]
+            result = {}
+            result['year'] = resp_data[1]
+            result['make'] = resp_data[2]
+            result['model'] = resp_data[3]
+            result['engine'] = resp_data[4]
+            datas = json.loads(resp_data[5])
 
-                        for model in models:
-                            try:
-                                data.update({"EquipmentModel":model['key']})
-                                result['model'] = model['text']
-                                resp_engines = self.get_response(data)
-                                engines = resp_engines.get('result',{}).get('columns',{}).get('EquipmentEngine',{}).get('results')
-                                
-                                for engine in engines:
-                                    try:
-                                        result['engine'] = engine['text']
-                                        res_data = {
-                                            "EquipmentClass":data['EquipmentClass'],
-                                            "EquipmentYear":data['EquipmentYear'],
-                                            "EquipmentMake":data['EquipmentMake'],
-                                            "EquipmentModel":data['EquipmentModel'],
-                                            "EquipmentEngine":engine['key'],
-                                            "source":"result",
-                                            "search_type":None,
-                                            "page":0
-                                        }
-                                        resp_data = self.get_response(res_data)
-                                        try:
-                                            result['response'] = json.dumps(resp_data)
-                                        except:
-                                            result['response'] = None
-                                        self.insert_datas(result)  
-                                    except Exception as ex:
-                                        print(ex) 
-                            except Exception as ex:
-                                print(ex)
-                    except Exception as ex:
-                        print(ex)
-            except Exception as ex:
-                print(ex)
+            datas = datas.get('result',{}).get('columns',{}).get('PartNumber',{}).get('results')
+            error = 1
+            for data in datas:
+                try:
+                    cat = data.get('props',{}).get('ProductCategory')
+                    if cat and cat == 'Belt Drive System':
+                        part = data.get('props',{}).get('ProductNr')
+                        if not part:
+                            continue
+                        result['Part_number'] = part
+                        result['Application'] = data.get('props',{}).get('ApplicationDescription')
+                        result['Product_Type'] = data.get('props',{}).get('ProductType')
+                        url = 'https://navigates.gates.com/us' + data.get('href')
+                        params = {
+                            'ProductNr': part,
+                            'BrandName': 'gates',
+                            'SearchType': 'vehicle',
+                            'Reset': '1',
+                        }
+                        response = self.get_response(params)
+                        resultsSpec = response.get('result',{}).get('prod',{}).get('Specs')
+                        if not resultsSpec:
+                            print('No resultsSpec. ', params)
+                            error += 1
+                            self.update_status(id, 4)
+                            continue
+                        specs = {}
+                        for spec in resultsSpec:
+                            specs[spec['Criteria']]=spec['Value']
 
-    def get_api_key(self):
+                        result.update({'Specs':specs})
+                        self.insert_datas(result, url)
+                except Exception as ex:
+                    print('ERROR: ',ex)
+                    error += 1
+                    self.update_status(id, 3)
+                    continue
+            if error == 1:
+                self.update_status(id, 2)
+
+    def update_status(self, id: int, status: int):
+        sql = f"UPDATE {self.model.table_tasks} SET status=%s WHERE id=%s"
+        self.model.insert(sql,(status,id))
+
+   
+    def insert_datas(self, res: dict, url: str):
+        sql = f"INSERT INTO {self.model.table_datas} (datas, URL) VALUES (%s,%s)"
+        print(res)
+        self.model.insert(sql, (json.dumps(res), url))
+        
+
+    def get_link_response(self, link: str, count_try: int = 0) -> str:
+        proxy = random.choice(self.list_proxies)
+        proxies = {
+            'http': proxy,
+            'https': proxy
+        }
+        try:
+            response = requests.get(link, 
+                                    headers=self.get_headers(),
+                                    proxies=proxies)
+
+            return response.text
+        except Exception as ex:
+            print(ex)
+        if count_try > 5:
+            raise Exception('no data')
+        return self.get_response(link, count_try+1)
+
+
+    def get_response(self, params: dict, count_try: int = 0) -> dict:
+        proxy = random.choice(self.list_proxies)
+        proxies = {
+            'http': proxy,
+            'https': proxy
+        }
+        try:
+            response = requests.get(
+                'https://api-v3-us.partsb2.com/api/Gates-US/Product/GetProductUSDetails',
+                params=params,
+                headers=self.get_headers(),
+                proxies=proxies
+            )
+            response.raise_for_status()
+            return response.json()
+        except Exception as ex:
+            print(ex)
+        if count_try > 5:
+            raise Exception('no data')
+        return self.get_response(params, count_try+1)
+
+    def get_headers(self) -> dict:
         headers = {
-            '0': '[object Object]',
-            'accept': '*/*',
-            'accept-language': 'en-US,en;q=0.9,en;q=0.9;q=0.8',
-            'content-type': 'application/json',
-            'dnt': '1',
+            'abp-tenantid': '36',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
+            'b2v4apikey': 'gatesus.92mcsQV5ietS8VlTnpRuA1mWbJ0EjGdL/gmeqD91g9o=',
+            'gates_country': 'USA',
+            'gates_object_id': 'E1079129285',
+            'gates_server_id': '3',
+            'origin': 'https://navigates.gates.com',
             'priority': 'u=1, i',
             'referer': 'https://navigates.gates.com/',
             'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
@@ -111,89 +153,14 @@ class GenerateTask():
             'sec-ch-ua-platform': '"macOS"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-        }
-        params = {
-            'd': str(int(time.time() * 1000)),
-        }
-        proxy = random.choice(self.list_proxies)
-        proxies = {
-            'http': proxy,
-            'https': proxy
-        }
-        response = requests.get(
-            'https://navigates.gates.com/us/assets/config/appconfig.production.json',
-            params=params,
-            proxies=proxies,
-            headers=headers,
-        )
-        data = response.json()
-        return data['remoteServiceApiKey']
-
-
-    def insert_datas(self, result: dict):
-        try:
-            print(result.get('year'), result.get('make'), result.get('model'), result.get('engine'))
-            value_placeholders = ', '.join(['%s'] * len(result)) 
-            sql = f"INSERT INTO {self.model.table_tasks} ({', '.join(result.keys())}) VALUES ({value_placeholders})"
-            self.model.insert(sql, list(result.values()))
-        except Exception as ex:
-            print(ex)
-
-    def get_response(self, data: dict, count_try: int = 0) -> dict:
-        headers = {
-            'abp-tenantid': '36',
-            'accept': 'text/plain',
-            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
-            'b2v4apikey': str(self.b2v4apikey),
-            'cache-control': 'no-cache',
-            'content-type': 'application/json-patch+json',
-            'expires': 'Sat, 01 Jan 2000 00:00:00 GMT',
-            'gates_country': 'USA',
-            'gates_object_id': 'E1079129285',
-            'gates_server_id': '3',
-            'origin': 'https://navigates.gates.com',
-            'pragma': 'no-cache',
-            'priority': 'u=1, i',
-            'referer': 'https://navigates.gates.com/',
-            'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'cross-site',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
             'x-requested-with': 'XMLHttpRequest',
         }
-        proxy = random.choice(self.list_proxies)
-        proxies = {
-            'http': proxy,
-            'https': proxy
-        }
-        try:
-            response = requests.post('https://api-v3-us.partsb2.com/api/Gates-US/Search/AutoSearchUS', 
-                                    headers=headers, 
-                                    data=json.dumps(data),
-                                    proxies=proxies)
-            response.raise_for_status()
-            return response.json()
-        except Exception as ex:
-            print(ex)
-            pass
-        if count_try > 20:
-            print('no data')
-            return None
-        return self.get_response(data, count_try+1)
+        return headers
 
 
-        
 
 if __name__ == "__main__":
-    IsDbCreated().check()
-    print('OK')
-    # GetTreadPagesContent().run()
-
-
-
-
+    # IsDbCreated().check()
+    TreadGеtProducts().run()
